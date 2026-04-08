fdbfc5cca529dd33bc2f6126caf108d438ae950b
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2026-04-08 16:22:45 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
 resource "random_pet" "name1" {
- length    = "1"
+ length    = "3"
  separator = "-"
 }
 
 resource "random_pet" "name2" {
- length    = "1"
+ length    = "3"
+ separator = "-"
+}
+
+resource "random_pet" "name3" {
+ length    = "3"
+ separator = "-"
+}
+
+resource "random_pet" "name4" {
+ length    = "3"
  separator = "-"
 }
